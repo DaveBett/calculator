acd0109755fce53c8971ba5a67c8da4d3d06dfa7
DaveBett <davide.betterini@gmail.com>
2025-03-17 21:27:51 +0000

    added operate function and comments

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+//Sum of a, b...
 function add(a, b) {
     return a + b;
 }
 
+//Subtraction of a, b...
 function subtract(a, b) {
     return a - b;
 }
 
+//Mutiply a, b...
 function multiply(a, b) {
     return a * b;
 }
 
+//Divide a, b...
 function divide(a, b) {
     return a / b;
+}
+
+let current = 0;
+let first = 0;
+let second = 0;
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
 }
