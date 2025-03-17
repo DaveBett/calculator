fd207a2cff6f6714bdf1f1eafe168f691fcc6b46
DaveBett <davide.betterini@gmail.com>
2025-03-17 22:25:16 +0000

    added functions to buttons, calculator works

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//DOM Elements
+const numberButton = document.querySelectorAll(".number");
+const operatorButton = document.querySelectorAll(".operator");
+const clearButton = document.querySelectorAll(".clear");
+const displayView = document.querySelector(".display");
+const resultButton = document.querySelector(".result");
+
+
 //Sum of a, b...
 function add(a, b) {
     return a + b;
@@ -34,3 +42,30 @@ function operate(num1, num2, operator) {
             return divide(num1, num2);
     }
 }
+
+let firstNumber = "";
+let storedNumber = "";
+let chosenOperator = "";
+let result = "";
+displayView.textContent = 0;
+
+numberButton.forEach((number) => { 
+    number.addEventListener("click", function() {
+        firstNumber += number.value;
+        displayView.textContent = firstNumber;
+    })
+});
+
+operatorButton.forEach((operator) => {
+    operator.addEventListener("click", function() {
+        chosenOperator = operator.value;
+        displayView.textContent = firstNumber + " " + chosenOperator;
+        storedNumber = firstNumber;
+        firstNumber = "";
+    })
+});
+
+resultButton.addEventListener("click", function() {
+    result = operate(parseInt(storedNumber), parseInt(firstNumber), chosenOperator);
+    displayView.textContent = result;
+});
